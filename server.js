33da6cc0a83e48705641c069bfd20a9820b777ad
Pujan Srivastava <psrivastava@ea.com>
2018-07-01 06:35:25 +0530

    Add Comments

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const [major, minor] = process.versions.node.split('.').map(parseFloat);
-if (major < 7 || (major === 7 && minor <= 5)) {
+if (major < 7 || (major === 7 && minor <= 5)) { // Project utilizes ES6 features: https://node.green
   console.log('older version of node');
   process.exit();
 }
 
 require('dotenv').config({ path: 'variables.env' });
 
+// Singleton DB Connection
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
@@ -35,12 +36,11 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', routes);
+app.use('/', routes); // entry point for controllers
 app.use(errorHandlers.notFound);
 
 if (app.get('env') === 'development') {
-    /* Development Error Handler - Prints stack trace */
-    app.use(errorHandlers.developmentErrors);
+    app.use(errorHandlers.developmentErrors);/* Development Prints stack trace */
 }
 // production error handler
 app.use(errorHandlers.productionErrors);
